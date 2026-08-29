b5ca093c3fa21d21777473457799ff71bb5b8d20
cagustavodev <gugucordeiroandrade@gmail.com>
2026-08-28 21:20:32 -0300

    adding update and delete API

--- src/app.ts
+++ src/app.ts
@@ -86,7 +86,7 @@ app.get("/categories/:id", (req, res) => {
     });
 
     if(!category){
-        return res.status(404).json({
+        return res.status(404).json({ // 404 = error
             message: "Categoria não encontrada"
         });
     }
@@ -94,29 +94,148 @@ app.get("/categories/:id", (req, res) => {
     res.status(200).json(category); 
 });
 
+//=============
+//post categories
+//=============
+
 app.post("/categories", (req, res) => {
     const category = {
         id: randomUUID(),
         ...req.body,
-    }
+    };
     
     categories.push(category);
     
     res.status(201).json(category); //201 é para POST, receber, no postman tem que salvar tudo 
 });
 
-//============
-// products
-//============
+//=============
+//put/update categories
+//=============
 
+//put = update
+app.put("/categories/:id", (req, res) => {
+    const category = categories.find((category) => {
+        return category.id == req.params.id;
+    });
+
+    if(!category){
+        return res.status(404).json({ // 404 = error
+            message: "Categoria não encontrada"
+        });
+    }
+
+    category.name = req.body.name;
+    category.description = req.body.description;
+
+    res.status(200).json(category); 
+});
+
+//=============
+//delete categories
+//=============
+
+app.delete("/categories/:id", (req, res) => {
+    const category = categories.find((category) => {
+        return category.id == req.params.id;
+    });
+
+    if(!category){
+        return res.status(404).json({ // 404 = error
+            message: "Categoria não encontrada"
+        });
+    }
+
+    const index = categories.indexOf(category);
+    categories.splice(index,1) //numero de elementos a remover
+
+    res.status(200).json({
+        message: "Categoria removida com sucesso "
+    }); 
+});
+
+//====================================
+//PRODUCTS
+//====================================
+
+//============
+// products get and post
+//============
 app.get("/products", (req, res) => {
     res.status(200).json(products);
 });
 
 app.post("/products", (req, res) => {
-    const product = req.body;
+    const product = {
+        id: randomUUID(),
+        ...req.body,
+    };
     products.push(product);
     res.status(201).json(product); //201 é para POST, receber, no postman tem que salvar tudo 
+});
+
+//===============
+//products by id
+//===============
+
+app.get("/products/:id", (req, res) => {
+    const product = products.find((product) => {
+        return product.id == req.params.id;
+    });
+
+    if(!product){
+        return res.status(404).json({
+            message: "Product não encontrada"
+        });
+    }
+
+    res.status(200).json(product); 
+});
+
+//===============
+//products update
+//===============
+
+app.put("/products/:id", (req, res) => {
+    const product = products.find((product) => {
+        return product.id == req.params.id;
+    });
+
+    if(!product){
+        return res.status(404).json({ // 404 = error
+            message: "Produto não encontrada"
+        });
+    }
+
+    product.name = req.body.name;
+    product.description = req.body.description;
+    product.price = req.body.price;
+    product.categoryId = req.body.categoryId;
+
+    res.status(200).json(product); 
+});
+
+//===============
+//products delete
+//===============
+
+app.delete("/products/:id", (req, res) => {
+    const product = products.find((product) => {
+        return product.id == req.params.id;
+    });
+
+    if(!product){
+        return res.status(404).json({ // 404 = error
+            message: "Produto não encontrado"
+        });
+    }
+
+    const index = products.indexOf(product);
+    products.splice(index,1) //numero de elementos a remover
+
+    res.status(200).json({
+        message: "Produto removido com sucesso "
+    }); 
 });
 
 export default app;
